b8e0ebf67675e53ee4afdc3429ae515c8f9f6f56
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-23 14:41:09 -0700

    using os module and exploring other modules in standard lib

--- standard_library.py
+++ standard_library.py
@@ -3,6 +3,7 @@ import random
 import math
 import datetime
 import calendar
+import os
 
 courses = ['history', 'math', 'science', 'art']
 
@@ -17,3 +18,9 @@ today = datetime.date.today
 print(today())
 
 print(calendar.isleap(2020))
+
+print(os.getcwd())   # this prints out the current working directory for where this file is located
+
+# we can also create/delete/search files and all of that usign the os module
+
+print(os.__file__)
